27312efe414ca2a684f3d7651a7b9a8e1602b6d8
Edwin Wu <edwin_wu05@126.com>
2017-03-11 23:33:31 +0800

    update 5-5 finish
    xadmin.views.BaseAdminView # 自定义主题设置
        enable_themes = True
        use_bootswatch = True
    
    xadmin.views.CommAdminView # 设置头和尾
        site_title = '慕学后台管理系统'
        site_footer = '慕学在线网'
        menu_style = 'accordion'  # 菜单样式

--- MxOnlie/apps/organization/adminx.py
+++ MxOnlie/apps/organization/adminx.py
@@ -4,27 +4,30 @@ __date__ = '2017/3/11 00:19'
 
 import xadmin
 
-from .models import Teacher
-from .models import CourseOrg
-from .models import CityDict
-
-
-class TeacherAdmin(object):
-    pass
-
-
-xadmin.site.register(Teacher, TeacherAdmin)
-
-
-class CourseOrgAdmin(object):
-    pass
-
-
-xadmin.site.register(CourseOrg, CourseOrgAdmin)
+from .models import CityDict, CourseOrg, Teacher
 
 
 class CityDictAdmin(object):
-    pass
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc', 'add_time']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address', 'city', 'add_time']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address', 'city', 'add_time']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address', 'city', 'add_time']
+
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums',
+                    'add_time']
+    search_fields = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums',
+                     'add_time']
+    list_filter = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums',
+                   'add_time']
 
 
 xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
--- MxOnlie/apps/users/adminx.py
+++ MxOnlie/apps/users/adminx.py
@@ -4,12 +4,36 @@ __date__ = '2017/3/10 18:37'
 
 import xadmin
 
-from .models import EmailVerityRecord
+from xadmin import views
+from .models import EmailVerityRecord, Banner
+
+
+# 自定义主题设置,导入xadmin.views.BaseAdminView,再进行注册
+class BassSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+# 设置头和尾,导入xadmin.views.CommAdminView,再进行注册
+class GlobalSettings(object):
+    site_title = '慕学后台管理系统'
+    site_footer = '慕学在线网'
+    menu_style = 'accordion'  # 菜单样式
 
 
 class EmailVerityRecordAdmin(object):
-    pass
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
 
 xadmin.site.register(EmailVerityRecord, EmailVerityRecordAdmin)
-
+xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BassSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
