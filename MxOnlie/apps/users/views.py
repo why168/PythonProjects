d321dc823eed57a85d1fcadaf8476f2d3da2d41c
Edwin Wu <edwin_wu05@126.com>
2017-03-19 18:59:29 +0800

    6-3 Finish
    AUTHENTICATION_BACKENDS = (
        'users.views.CustomBackend',
    )
    
    from django.contrib.auth.backends import ModelBackend
    from django.db.models import Q

--- MxOnlie/apps/users/views.py
+++ MxOnlie/apps/users/views.py
@@ -1,5 +1,20 @@
+# _*_ encoding:utf-8 _*_
+
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+from .models import UserProfile
+
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
 
 
 # Create your views here.
@@ -13,7 +28,7 @@ def user_login(request):
             login(request, user)
             return render(request, 'index.html')
         else:
-            return render(request, 'login.html')
+            return render(request, 'login.html', {'msg': u'用户名或密码错误!'})
         pass
     elif request.method == 'GET':
         return render(request, 'login.html', {})
