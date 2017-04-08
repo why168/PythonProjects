f8c5311343f239b2be776aab2315def9523f5a67
Edwin Wu <edwin_wu05@126.com>
2017-04-08 11:16:58 +0800

    7-5 Finish
    pip install django-pure-pagination
    分页功能

--- MxOnlie/apps/organization/views.py
+++ MxOnlie/apps/organization/views.py
@@ -4,6 +4,8 @@ from django.views.generic import View
 
 # Create your views here.
 from organization.models import CourseOrg, CityDict
+from django.shortcuts import render_to_response
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 
 class OrgView(View):
@@ -18,8 +20,17 @@ class OrgView(View):
         # 城市
         all_citys = CityDict.objects.all()
 
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 1, request=request)
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            'all_orgs': all_orgs,
+            'all_orgs': orgs,
             'all_citys': all_citys,
-            'org_nums': org_nums
+            'org_nums': org_nums,
         })
